88cb22aa74681ad5c191962484358236d1f28887
Nick Eaket <neaket@gmail.com>
2014-01-06 22:10:06 -0500

    Added FoodService Not-Implemented

---
+++ src/wp7/FSync/FSync/Services/FoodService.cs
@@ -0,0 +1,49 @@
+﻿using FSync.Models;
+
+namespace FSync.Services
+{
+    public class FoodService
+    {
+        public void Log(Food food)
+        {
+
+
+        }
+
+        public void Delete(Food food)
+        {
+
+        }
+
+        public void Recent()
+        {
+
+        }
+
+        public void Frequent()
+        {
+        }
+
+        public void Favorite()
+        {
+        }
+
+        public void AddFavorite()
+        {
+        }
+
+        public void DeleteFavorite()
+        {
+        }
+
+        public void CreateFood()
+        {
+
+        }
+
+        public void Goals()
+        {
+
+        }
+    }
+}
